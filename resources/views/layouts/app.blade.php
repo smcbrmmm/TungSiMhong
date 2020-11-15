d89c6d439589c974ea94dd864be18603b6f9135e
SzopA-26 <patiphat.kh@ku.th>
2020-11-15 18:24:28 +0700

    edit title name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>KU Residential</title>
+    <title>TungSiMhong</title>
 
     <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
