f0cc688933d464389f1ae7fc64df23fdf87e010b
Sirawit Yuwasirinun <nobsirawit@gmail.com>
2020-11-20 02:44:47 +0700

    seed data and add orderDetailSeeder

--- database/seeders/AddressSeeder.php
+++ database/seeders/AddressSeeder.php
@@ -35,5 +35,61 @@ class AddressSeeder extends Seeder
         $address->house_no = '54/10';
         $address->address ='ม.7 ต.ทุ่งมะพร้าว อ.ท้ายเหมือง';
         $address->save();
+
+        $address = new Address();
+        $address->user_id = 4;
+        $address->receiver_name = 'Sirawit Yuwasirinun';
+        $address->place_name = 'บ้าน';
+        $address->receiver_tel = '065-7925111';
+        $address->postal = '72140';
+        $address->province = 'สุพรรรณบุรี';
+        $address->house_no = '218/1';
+        $address->address ='ม.1 ต.วังน้ำซับ อ.ศรีประจันต์';
+        $address->save();
+
+        $address = new Address();
+        $address->user_id = 5;
+        $address->receiver_name = 'ทองริด ประสิทธ์ซับ';
+        $address->place_name = 'บ้าน';
+        $address->receiver_tel = '087-9025110';
+        $address->postal = '11150';
+        $address->province = 'นนทบุรี';
+        $address->house_no = '198/125';
+        $address->address ='ม.1 ต.ขุนศรี อ.ไทรน้อย';
+        $address->save();
+
+
+        $address = new Address();
+        $address->user_id = 5;
+        $address->receiver_name = 'Kotchakorn Prasitsub';
+        $address->place_name = 'บ้านแม่';
+        $address->receiver_tel = '091-8125110';
+        $address->postal = '25000';
+        $address->province = 'ปราจีนบุรี';
+        $address->house_no = '33/91';
+        $address->address ='ม.3 ต.ท่างาม	 อ.เมืองปราจีนบุรี';
+        $address->save();
+
+        $address = new Address();
+        $address->user_id = 5;
+        $address->receiver_name = 'ประเวศ ประสิทธ์ซับ';
+        $address->place_name = 'บ้านปู่';
+        $address->receiver_tel = '065-3232810';
+        $address->postal = '62160';
+        $address->province = 'ปราจีนบุรี';
+        $address->house_no = '33/91';
+        $address->address =' ต.ธำมรงค์  อ.เมืองกำแพงเพชร';
+        $address->save();
+
+        $address = new Address();
+        $address->user_id = 6;
+        $address->receiver_name = 'ไกร บุญไชโย';
+        $address->place_name = 'อาคารเมธีบำรุง';
+        $address->receiver_tel = '0871020251';
+        $address->postal = '67270';
+        $address->province = 'เพชรบูรณ์';
+        $address->house_no = '2/208';
+        $address->address =' ต.เขาค้อ  อ.เขาค้อ';
+        $address->save();
     }
 }
--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -16,5 +16,41 @@ class OrderSeeder extends Seeder
     public function run()
     {
 
+        $order = new Order();
+        $order->address_id = 1;
+        $order->user_id = 1;
+        $order->order_code = "PSD125958";
+        $order->order_status = "ตะกร้า";
+        $order->save();
+
+        $order = new Order();
+        $order->address_id = 2;
+        $order->user_id = 2;
+        $order->order_code = "SUP126958";
+        $order->order_status = "ตะกร้า";
+        $order->save();
+
+        $order = new Order();
+        $order->address_id = 2;
+        $order->user_id = 2;
+        $order->order_code = "OOU126058";
+        $order->order_status = "รอการชำระเงิน";
+        $order->save();
+
+        $order = new Order();
+        $order->address_id = 4;
+        $order->user_id = 5;
+        $order->order_code = "YYT126058";
+        $order->order_status = "รอการชำระเงิน";
+        $order->save();
+
+        $order = new Order();
+        $order->address_id = 7;
+        $order->user_id = 6;
+        $order->order_code = "YSS216058";
+        $order->order_status = "ตะกร้า";
+        $order->save();
+
+
     }
 }
